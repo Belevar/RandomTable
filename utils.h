731d2f623b1d3d776b29f7fd7b0849b809320fad
Radek <tlustywalec@gmail.com>
2014-01-15 20:48:41 +0100

    Dodanie funckji wyświetlającej show()

--- utils.h
+++ utils.h
@@ -1,9 +1,9 @@
 #include <vector>
 #include <algorithm>
 
-std::vector create()
+std::vector<int> create()
 {
-   std::vector v(10);
+   std::vector<int> v(10);
    
    for(int i = 0; i < 10; ++i)
    {
@@ -12,3 +12,14 @@ std::vector create()
 
    return v;
 }
+
+void show(const std::vector<int>& v)
+{
+	for(int i = 0; i < v.size(); ++i)
+	{
+		std::cout << v[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+
